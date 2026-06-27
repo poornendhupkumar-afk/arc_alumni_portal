c683cc6591306ce69f1b271bb3a3aebc3806d545
poornendhupkumar-afk <poornendhupkumar@gmail.com>
2026-06-27 18:37:17 +0530

    Initial commit

--- script.js
+++ script.js
@@ -578,3 +578,29 @@ function erase() {
 }
 
 window.addEventListener("load", type);
+document.querySelectorAll(".ripple").forEach(button => {
+
+    button.addEventListener("click", function(e){
+
+        const circle = document.createElement("span");
+
+        const diameter = Math.max(this.clientWidth, this.clientHeight);
+        const radius = diameter / 2;
+
+        circle.style.width = circle.style.height = `${diameter}px`;
+
+        circle.style.left =
+            `${e.clientX - this.getBoundingClientRect().left - radius}px`;
+
+        circle.style.top =
+            `${e.clientY - this.getBoundingClientRect().top - radius}px`;
+
+        this.appendChild(circle);
+
+        setTimeout(() => {
+            circle.remove();
+        }, 600);
+
+    });
+
+});
